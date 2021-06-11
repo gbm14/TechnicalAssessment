9a743cbac7673a52462643958b8b7a2be377856b
gsbo <gsbo@gft.com>
2021-06-11 21:33:52 +0200

    Created connection with database and general controller

---
+++ TechnicalAssessment/Controllers/BookmarksController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TechnicalAssessment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TechnicalAssessment.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookmarksController : ControllerBase
+    {
+        private readonly BookmarksContext _context;
+
+        private readonly ILogger<BookmarksController> _logger;
+
+        public BookmarksController(BookmarksContext context, ILogger<BookmarksController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        #region snippet
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Bookmarks>>> GetTodoItems()
+        {
+            return await _context.Bookmarks.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Bookmarks>> GetBookmark(long id)
+        {
+            var bookmark = await _context.Bookmarks.FindAsync(id);
+
+            if (bookmark == null)
+            {
+                return NotFound();
+            }
+
+            return bookmark;
+        }
+
+        #endregion snippet
+    }
+}
---
+++ TechnicalAssessment/Models/BookmarksContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TechnicalAssessment.Models
+{
+    public class BookmarksContext : DbContext
+    {
+        public BookmarksContext(DbContextOptions<BookmarksContext> options)
+                : base(options)
+        {
+        }
+
+        public DbSet<Bookmarks> Bookmarks { get; set; }
+    }
+}
